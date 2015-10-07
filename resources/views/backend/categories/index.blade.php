daf33050d48f6562050f7b4e221693233a863187
faycal1 <hasbellaoui.faycal@gmail.com>
2015-10-07 16:07:12 +0100

    =add quiz view

--- resources/views/backend/categories/index.blade.php
+++ resources/views/backend/categories/index.blade.php
@@ -3,16 +3,18 @@
 @section('page-header')
     <h1>
         Quiz (Qcm) Application
-        <small>{{ trans('strings.backend.dashboard_title') }}</small>
+        <small>Gestion des cours et quizs</small>
     </h1>
 @endsection
 
 @section('breadcrumbs')
     <li><a href="{!!route('backend.dashboard')!!}"><i class="fa fa-dashboard"></i> {{ trans('menus.dashboard') }}</a></li>
-    <li class="active">{{ trans('strings.here') }}</li>
+    <li class="active">Gestion des Categories</li>
 @endsection
 
 @section('content')
+    @include('backend.access.includes.partials.header-buttons')
+
     <div class="box box-success">
         <div class="box-header with-border">
           <h3 class="box-title">{{ trans('strings.backend.WELCOME') }} {!! auth()->user()->name !!}!</h3>
