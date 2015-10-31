a5f9b6a431c24528d62712aabba902479315d74d
faycal1 <hasbellaoui.faycal@gmail.com>
2015-10-31 20:16:53 +0100

    App Key Genration

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -2,7 +2,7 @@
           <footer class="main-footer">
             <!-- To the right -->
             <div class="pull-right hidden-xs">
-                <a href="http://laravel-boilerplate.com" target="_blank">{{ trans('strings.boilerplate_link') }}</a>
+                <a href="http://offrine.com" target="_blank">Offrine</a>
             </div>
             <!-- Default to the left -->
             <strong>Copyright &copy; 2015 <a href="#">Company</a>.</strong> {{ trans('strings.all_rights_reserved') }}
